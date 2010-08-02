2e9b13194f17888a5e700ad078258231df73654d
aaron <aaronbs@gmail.com>
2010-08-02 13:34:37 -0400

    first commit

---
+++ semantic/lib/VipContentHandler.java
@@ -0,0 +1,129 @@
+import java.io.IOException;
+import org.xml.sax.Attributes;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.Locator;
+import org.xml.sax.SAXException;
+
+//from: http://java.sun.com/developer/Books/xmljava/ch03.pdf
+class VipContentHandler implements org.xml.sax.ContentHandler {
+
+        /** Hold onto the locator for location information */
+        private Locator locator;
+        private SemVal sv;
+
+        private String topLevelType;
+        private Long topLevelId;
+        private String lowLevelType;
+        private boolean addressMode=false;
+        private String content;
+        
+        private static String topTypes = "|street_segment|precinct|precinct_split|contest|ballot|candidate|electoral_district|" + 
+        "polling_location|early_vote_site|election_administration" + "|locality|state|" + "source|election|" + "ballot|candidate|referendum|ballot_response|custom_ballot|";
+        private static String allTypes = VipContentHandler.topTypes + "precinct_id|precinct_split_id|electoral_district_id|" + 
+                "polling_location_id|early_vote_site_id|election_administration_id|eo_id|ovc_id|" + "locality_id|state_id|" + "feed_contact_id|" +
+                "ballot_id|candidate_id|referendum_id|ballot_response_id|custom_ballot_id|";
+        private static String addressTypes = "|physical_address|mailing_address|address|non_house_address|filed_mailing_address|";
+                
+        public VipContentHandler(SemVal sv) {
+                super();
+                this.sv = sv;
+        }
+
+        //localName is the important one
+        public void startElement(String namespaceURI, String localName, String rawName, Attributes atts) throws SAXException {
+                if (sv.getErrorCount() < SemVal.maxErrors) {
+                        String rawNameChk = "|" + rawName + "|";
+                        if (VipContentHandler.addressTypes.contains(rawNameChk)) {
+                                addressMode=true;
+                        }
+                        if (!addressMode) {
+                                if (VipContentHandler.allTypes.contains(rawNameChk)) {
+                                        if (VipContentHandler.topTypes.contains(rawNameChk)) {
+                                                topLevelType = rawName;
+                                                //System.out.println("Top level is: " + topLevelType);
+                
+                                                String attrName = atts.getQName(0);
+                                                if (attrName == "id") {
+                                                        Long idLong = new Long(atts.getValue(0));
+                                                        topLevelId = idLong;
+                                                        //System.out.println("Adding ID " + idLong + " of type " + topLevelType);
+                                                        sv.addIdType(idLong, rawName, locator.getLineNumber());
+                                                } else {
+                                                        sv.addError("First attribute for " + rawName + " at line " + locator.getLineNumber() + " is " + attrName);
+                                                }
+                                        } else { //connector
+                                                lowLevelType = rawName;
+                                        }
+                                }
+                        }
+                        content="";
+               }
+        }
+
+        public void characters(char[] ch, int start, int end) throws SAXException {
+                if (lowLevelType != null && sv.getErrorCount() < SemVal.maxErrors) {
+                        content += new String(ch, start, end);
+                } else {
+                        content="";
+                }
+        }
+
+        public void endElement(String namespaceURI, String localName, String rawName) throws SAXException {
+                if (sv.getErrorCount() < SemVal.maxErrors) {
+                        String rawNameChk = "|" + rawName + "|";
+                        //System.out.println(namespaceURI + " " + rawName + " " + rawName);
+                        if (!addressMode) {
+                                if (VipContentHandler.allTypes.contains(rawNameChk)) {
+                                        if (VipContentHandler.topTypes.contains(rawNameChk)) {
+                                                topLevelType = null;
+                                                //System.out.println("Top level is null");
+                                                topLevelId = null;
+                                        } else {
+                                                if (content==null || content.equals("")) {
+                                                        sv.addError("Content of linking element " + lowLevelType + " at line " + locator.getLineNumber() +  "is blank.");
+                                                }
+                                                try {
+                                                        Long toId = Long.parseLong(content);
+                                                        //if (s.equals("440001405") || toId.equals(1405)) {
+                                                        if (topLevelId.equals(Long.decode("88000136340"))) {
+                                                                System.out.println("Gotcha: "+ topLevelId + " " + content + " " + toId);
+                                                        }
+                                                        //System.out.println("Linking element " + topLevelType + ":" + lowLevelType + " at line " + locator.getLineNumber());
+                                                        sv.addLink(topLevelType, topLevelId, locator.getLineNumber(), lowLevelType, toId);
+                                                } catch (Exception e) {
+                                                        sv.addError("Content of linking element " + lowLevelType + " at line " + locator.getLineNumber() +  " is not a (parseable) number: " + content);
+                                                }
+                                                lowLevelType = null;
+                                        }
+                                }
+                        }
+                        if (VipContentHandler.addressTypes.contains(rawNameChk)) {
+                                addressMode=false;
+                        }
+                }
+        }
+
+        public void startDocument() throws SAXException {
+        }
+        public void endDocument() throws SAXException {
+        }
+        public void processingInstruction(String target,
+                String data) throws SAXException {
+        }
+        public void startPrefixMapping(String prefix, String uri) {
+        }
+        public void endPrefixMapping(String prefix) {
+        }
+
+        public void ignorableWhitespace(char[] ch, int start,
+                int end) throws SAXException {
+        }
+        public void skippedEntity(String name) throws SAXException {
+        }
+
+        public void setDocumentLocator(Locator locator) {
+                // We save this for later use if desired.
+                this.locator = locator;
+        }
+
+}
---
+++ semantic/lib/VipSemChecker.java
@@ -0,0 +1,247 @@
+import java.awt.*;
+import javax.swing.*;
+import java.io.*;
+import java.net.*;
+import javax.jnlp.*;
+import java.awt.event.*;
+
+public class VipSemChecker {
+
+        private static TextField tf1;
+        private static TextField tf2;
+        private static TextArea ta1;
+        private static Choice c1;
+        private static FileContents localXMLFC = null;
+        private static JFrame frame;
+
+        public static void main(String args[]) {
+
+                frame = new JFrame("VIP Semantic Checker");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                Container contentPane = frame.getContentPane();
+                SpringLayout layout = new SpringLayout();
+                frame.setLayout(layout);
+
+                int cols = 60;
+                Label label1 = new Label("XML File");
+                Label label2 = new Label("XSD File");
+                Label label3 = new Label("Result:");
+                Label label4 = new Label("(Full file path)");
+                tf1 = new TextField(cols);
+                tf2 = new TextField(cols);
+                //tf2.setEditable(false);
+                tf2.setEditable(true);
+                Button b1 = new Button("Open File");
+                c1 = new Choice();
+                //c1.add("v2.2");
+                c1.add("v2.1");
+                c1.add("v2.0");
+                //c1.add("v1.5");
+                updateXSDTF();
+
+                Button b3 = new Button("Check File Syntax and Semantics");
+                ta1 = new TextArea(25, cols + 10);
+                ta1.setEditable(false);
+                int xPadInt = 5;
+                Spring xPad = Spring.constant(xPadInt);
+                int yPadInt = 5;
+                Spring yPad = Spring.constant(yPadInt);
+
+
+
+                layout.putConstraint(SpringLayout.WEST, label1,
+                        xPadInt, SpringLayout.WEST, contentPane);
+                layout.putConstraint(SpringLayout.NORTH, label1,
+                        yPadInt, SpringLayout.NORTH, contentPane);
+
+                layout.putConstraint(SpringLayout.WEST, tf1, xPadInt,
+                        SpringLayout.EAST, label1);
+                layout.putConstraint(SpringLayout.NORTH, tf1, yPadInt,
+                        SpringLayout.NORTH, contentPane);
+
+                layout.putConstraint(SpringLayout.WEST, b1, xPadInt,
+                        SpringLayout.EAST, tf1);
+                layout.putConstraint(SpringLayout.NORTH, b1, yPadInt,
+                        SpringLayout.NORTH, contentPane);
+                layout.putConstraint(SpringLayout.WEST, label4, xPadInt,
+                        SpringLayout.EAST, tf1);
+                layout.putConstraint(SpringLayout.NORTH, label4, yPadInt,
+                        SpringLayout.NORTH, contentPane);                        
+
+                layout.putConstraint(SpringLayout.WEST, label2,
+                        xPadInt, SpringLayout.WEST, contentPane);
+                layout.putConstraint(SpringLayout.NORTH, label2,
+                        yPadInt, SpringLayout.SOUTH, label1);
+
+                layout.putConstraint(SpringLayout.WEST, tf2, xPadInt,
+                        SpringLayout.EAST, label2);
+                layout.putConstraint(SpringLayout.NORTH, tf2, yPadInt,
+                        SpringLayout.SOUTH, tf1);
+
+                layout.putConstraint(SpringLayout.WEST, c1, xPadInt,
+                        SpringLayout.EAST, tf2);
+                layout.putConstraint(SpringLayout.NORTH, c1, yPadInt,
+                        SpringLayout.SOUTH, b1);
+                layout.putConstraint(SpringLayout.NORTH, c1, yPadInt,
+                        SpringLayout.SOUTH, label4);
+                        
+                layout.putConstraint(SpringLayout.WEST, b3, xPadInt,
+                        SpringLayout.WEST, contentPane);
+                layout.putConstraint(SpringLayout.NORTH, b3, yPadInt,
+                        SpringLayout.SOUTH, label2);
+
+                layout.putConstraint(SpringLayout.WEST, label3,
+                        xPadInt, SpringLayout.WEST, contentPane);
+                layout.putConstraint(SpringLayout.NORTH, label3,
+                        yPadInt, SpringLayout.SOUTH, b3);
+
+                layout.putConstraint(SpringLayout.WEST, ta1, xPadInt,
+                        SpringLayout.EAST, label2);
+                layout.putConstraint(SpringLayout.NORTH, ta1, yPadInt,
+                        SpringLayout.SOUTH, b3);
+
+
+                contentPane.add(label1);
+                contentPane.add(tf1);
+                //contentPane.add(b1);
+                contentPane.add(label4);
+                contentPane.add(label2);
+                contentPane.add(tf2);
+                contentPane.add(c1);
+                contentPane.add(label3);
+                contentPane.add(ta1);
+                contentPane.add(b3);
+
+
+                Spring maxHeightSpring = Spring.constant(0);
+                Spring maxWidthSpring = Spring.constant(0);
+                Component[] components = contentPane.getComponents();
+                for (int i = 0; i < components.length; i++) {
+                        SpringLayout.Constraints cons =
+                                layout.getConstraints(components[i]);
+                        maxWidthSpring = Spring.max(maxWidthSpring,
+                                cons.getConstraint("East"));
+                        maxHeightSpring = Spring.max(maxHeightSpring,
+                                cons.getConstraint("South"));
+                }
+                SpringLayout.Constraints pCons =
+                        layout.getConstraints(contentPane);
+                pCons.setConstraint("East", Spring.constant(650));
+                pCons.setConstraint("South", Spring.constant(500));
+
+
+                frame.pack();
+                frame.setVisible(true);
+
+                /* Do stuff */
+                b1.addActionListener(new ActionListener() {
+                             public void actionPerformed(ActionEvent e) {
+
+                                     FileOpenService fos;
+
+                             try {
+                                             fos = (FileOpenService) ServiceManager.lookup("javax.jnlp.FileOpenService");
+                             } catch (
+                                     UnavailableServiceException uexc) {
+                                             fos = null;
+                                     }
+
+                             if (fos != null) {
+                                     try {
+                                     // ask user to select a file through this service
+                                     String[] exts = new String[1];
+                                     exts[0]="xml";
+                                     FileContents fc = fos.openFileDialog(null, exts);
+                                             if (fc !=  null) {
+                                                     tf1.setText(fc.getName());
+                                                     localXMLFC = fc;
+                                             }
+
+                                     } catch (Exception exc) {
+                                                     exc.printStackTrace();
+                                             }
+                                     }
+
+
+                             }
+                     }
+                    );
+
+                c1.addItemListener(new ItemListener() {
+                                           public void itemStateChanged(
+                                                   ItemEvent e) {
+                                                   updateXSDTF();
+                                           }
+                                   }
+                                  );
+
+                b3.addActionListener(new ActionListener() {
+                                             public void actionPerformed(
+                                                     ActionEvent e) {
+                                                     validate();
+                                             }
+                                     }
+                                    );
+
+        }
+
+        private static void updateXSDTF() {
+                tf2.setText(
+                        "http://election-info-standard.googlecode.com/files/vip_spec_" +
+                        c1.getSelectedItem() + ".xsd");
+        }
+
+        private static void validate() {
+                SemVal sv = null;
+                boolean go = false;
+                //System.out.println(tf1.getText().length());
+                if (localXMLFC != null) {
+                        sv = new SemVal(localXMLFC, tf2.getText());
+                        go = true;
+                } else if (tf1.getText().length() > 0) {
+                        sv = new SemVal(tf1.getText(), tf2.getText());
+                        go = true;
+                } else {
+                        ta1.setText("Please pick an XML file." + "\n");
+                }
+
+                if (go) {
+                        ta1.setText(
+                                "Ready for syntactic and semantic validation..." +
+                                "\n");
+                        toggleCursor(true);
+                        String msg = sv.validateSyntax();
+                        appendMessage(msg);
+                        if (sv.isSynValid()) {
+                                appendMessage("\n------------------------------------------------\n");
+                                msg = sv.validateSemantics();
+                                appendMessage(msg);
+                        }
+                        toggleCursor(false);
+                }
+        }
+
+        public static void appendMessage(String msg) {
+                ta1.setText(ta1.getText() + msg + "\n");
+        }
+
+        private static void toggleCursor(boolean waiting) {
+                if (waiting) {
+                        frame.setCursor( Cursor.getPredefinedCursor(
+                                Cursor.WAIT_CURSOR));
+                } else {
+                        frame.setCursor(Cursor.getDefaultCursor());
+                }
+        }
+
+        public static boolean isAlive() {
+                return(frame != null);
+        }
+
+}
+
+//D:\google\spec\sample feed for v2.0.xml.txt
+//D:\google\state data\NC\vipFeed-37-2008-10-05T18-07-18.xml
+//file://localhost/D:/google/spec/vip_spec_v2.0.xsd
+
+
